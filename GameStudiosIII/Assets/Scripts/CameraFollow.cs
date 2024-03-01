c14f5d854db4e753f4560d0996ebd522443f7970
mateusdaemon <mateusjdm@gmail.com>
2024-02-29 22:13:28 -0300

    Rotate da camera

--- GameStudiosIII/Assets/Scripts/CameraFollow.cs
+++ GameStudiosIII/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    private float zOffset = 4;
+    public float zOffset = 0;
 
     // Start is called before the first frame update
     void Start()
